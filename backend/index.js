754340006b6eea69b1d251532dc6820ef91ca9ed
zademanish <manishzademz7@gmail.com>
2025-01-30 19:41:44 +0530

    updatte urls

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://netflix-clone-git-master-manish-zades-projects.vercel.app/',
+    origin:'https://netflix-clone-tau-flax-18.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions));
