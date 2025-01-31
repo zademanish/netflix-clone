2126500eaa1a8e04b3744aef47bf9b9362779b0c
zademanish <manishzademz7@gmail.com>
2025-01-31 14:25:44 +0530

    add backend url

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "https://netflix-clone-backend-nine.vercel.app/api/v1/user";
+export const API_END_POINT = "https://netflix-clone-backend-ten.vercel.app/api/v1/user";
 
 export const options = {
     method: 'GET',
